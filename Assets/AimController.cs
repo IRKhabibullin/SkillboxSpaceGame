deaa79c9b0efa247c3e452ee4237177d4f2bf2a8
IRKhabibullin <irkhabibullin@gmail.com>
2020-07-22 20:18:57 +0300

    added aim

---
+++ Assets/AimController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimController : MonoBehaviour {
+    private LineRenderer aim_line;
+    private Transform mainGun;
+
+    void Start() {
+        aim_line = GetComponent<LineRenderer>();
+        aim_line.positionCount = 2;
+
+        mainGun = GameObject.Find("MainGun").transform;
+        aim_line.SetPosition(0, mainGun.position);
+    }
+
+    void Update() {
+    	aim_line.SetPosition(0, mainGun.position);
+    	RaycastHit hit;
+        if (Physics.Raycast(mainGun.position, mainGun.forward, out hit, 200, LayerMask.GetMask("Asteroid", "Ship"))) {
+        	aim_line.SetPosition(1, hit.point);
+        } else {
+        	aim_line.SetPosition(1, mainGun.position + mainGun.forward * 200);
+        }
+    }
+}
